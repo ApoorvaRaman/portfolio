3b63a80faf8f74f66b6e1a13a95787d8a7f48ee4
Apoorva Raman <apoorvaramans@gmail.com>
2026-01-26 21:18:58 +0530

    Created Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,20 @@
+import Section from "./Section";
+export default function Skills() {
+  return (
+    <Section title="Skills">
+      <p>
+        <strong>Languages:</strong> C (strong), Python, Java
+      </p>
+      <p>
+        <strong>Foundations:</strong> Data Structures, Algorithms, Theory of
+        Computation, OOP
+      </p>
+      <p>
+        <strong>Frontend:</strong> HTML, CSS
+      </p>
+      <p>
+        <strong>Tools:</strong> Git, PowerBI, Figma
+      </p>
+    </Section>
+  );
+}
