a2d6a524c426adfc8e204904de7d2b53e8f77e2a
Apoorva Raman <apoorvaramans@gmail.com>
2026-01-28 19:27:17 +0530

    Created useTheme.js

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+export default function useTheme() {
+  const getInitialTheme = () => {
+    const saved = localStorage.getItem("theme");
+    if (saved) return saved;
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  };
+  const [theme, setTheme] = useState(getInitialTheme);
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+  return [theme, setTheme];
+}
